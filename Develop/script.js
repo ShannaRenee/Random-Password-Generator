a649fb93372866250c26352054eb35efa9f78daa
Calcifer <shanna.renee.morris@gmail.com>
2023-04-05 13:01:59 -0700

    making the code look nice

--- Develop/script.js
+++ Develop/script.js
@@ -2,106 +2,80 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
-
 // // Write password to the #password input
 function writePassword() {
 
-  // First prompt - Length of Password
-   var passwordLength = prompt("How long would you like your password to be?", "(select between 8-128 characters)");
+  // Part 1 - preferences selected by user
+    //Input validations
+   var passwordLength = prompt("How long would you like your password to be?", "(select between 8-128 characters)"); {
     if (passwordLength < 8) {
-        alert("Not enough characters: Please enter a number between 8 and 128.");
+        alert("Not enough characters: Please enter a number between 8 and 128."); 
         writePassword();
+        return;
 
     } else if (passwordLength > 128) {
         alert("Too many characters: Please enter a number between 8 and 128.");
         writePassword();
-    }
+        return;
 
+    } else if (passwordLength === "(select between 8-128 characters)") {
+        alert("Oops! You didn't type anything.\nPlease try again.");
+        writePassword();
+        return;
 
-  // First condition - lowercase letters
-    var lowerCase = confirm("Would you like your password to contain lowercase letters?");
-      
-        
-    
-    // Second condition - uppercase letters
-    var upperCase = confirm("Would you like your password to contain uppercase letters?");
-    
-       
-    // Third condition - numbers
-    var numbers = confirm("Would you like your password to contain numbers?");
-    
-     
-
-    // Fourth condition - Special characters
-    var spChar = confirm("Would you like your password to contain special characters?");
-
-
-     
-
-    var characters = [];
-        
+    } else if (isNaN(passwordLength)) {
+        alert("That's not a number. \nPlease try again.");
+        writePassword();
+        return;
+    }}
   
-
-    var conditions = new Array ();
-    conditions[0] = new Array (lowerCase, ['abcdefghijklmnopqrstuvwxyz']);
-    conditions[1] = new Array (upperCase, ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']);
-    conditions[2] = new Array (numbers, ["0123456789"]);
-    conditions[3] = new Array (spChar, ["~`!@#$%^&*_-+={[}]|:;"]);
-
-    for (var i = 0; i < conditions.length; i++)
+var lowerCase = confirm("Would you like your password to contain lowercase letters?");
+      
   
-      if (conditions[i][0] === true) {
+var upperCase = confirm("Would you like your password to contain uppercase letters?");
+    
+    
+var numbers = confirm("Would you like your password to contain numbers?");
+    
+    
+var spChar = confirm("Would you like your password to contain special characters?");
 
-       
-        characters.push(conditions[i][1] + []);
-        
-      }
+
+     
+// Part 2 - Selected arrays will be joined together
+var characters = [];
+      
+var conditions = new Array ();
+conditions[0] = new Array (lowerCase, ['abcdefghijklmnopqrstuvwxyz']);
+conditions[1] = new Array (upperCase, ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']);
+conditions[2] = new Array (numbers, ["0123456789"]);
+conditions[3] = new Array (spChar, ["~`!@#$%^&*_-+={[}]|:;"]);
+
+
+  for (var i = 0; i < conditions.length; i++)
+  
+     if (conditions[i][0] === true) {
+       characters.push(conditions[i][1] + []);
+     }
       
 
-        
- var newCharacters = (characters.join(''));
+//Part 3 - New array is randomly selected from to create password
+var newCharacters = (characters.join(''));
  
-    function generatePassword(length) {
-          let result = " ";
-          const charactersLength = newCharacters.length;
-          for ( let i = 0; i < length; i++ ) {
-            result += newCharacters.charAt(Math.floor(Math.random() * charactersLength));
-          }
+function passwordGen(length) {
+    let password = " ";
+    const charactersLength = newCharacters.length;
+    for ( let i = 0; i < length; i++ ) {
+      password += newCharacters.charAt(Math.floor(Math.random() * charactersLength));
+    }
           
-          alert("This is your new password " + result );
-         return result;{
-    
-    
-    
-        }}
-          
-        generatePassword(passwordLength);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  }
 
-
-// }
-
-
-
-
-
-
-
-
-
-
+  passwordGen(passwordLength);
 }
-
-
-
-
-
-
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-  
     
+  
 // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword); 
+  generateBtn.addEventListener("click", writePassword); 
